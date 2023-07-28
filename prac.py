4b6612190cab12378eaa9cec6047c2b895ceba66
SyedMazhar6746 <syedmazhar6746@gmail.com>
2023-07-28 21:27:17 +0200

    prac.py

---
+++ prac.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python3
+
+print(2)
+
+print(3)
+print(3)
+print(2)
+print(2)
+print(23)
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+def mul(x, y):
+    return x*y
+
+
+
+def add_2(x,y):
+    return 2(x+y)
+def div(x,y):
+    return x/y
+
+def fn():
+    pass
+
+print(2)
