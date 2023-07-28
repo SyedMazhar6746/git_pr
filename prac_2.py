47944e7d62f3375a2fc85a416998d9ecd926611d
SyedMazhar6746 <syedmazhar6746@gmail.com>
2023-07-28 21:35:49 +0200

    added two files

---
+++ prac_2.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python3
+
+print(2)
+
+
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+def mul(x, y):
+    return x*y
+
+
+
+def add_2(x,y):
+    return 2(x+y)
+def div(x,y):
+    return x/y
+
+def fn():
+    pass
+
